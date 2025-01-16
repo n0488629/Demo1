517bcf3beab3db6f3d0283116a58a9ff14458f2f
n0488629 <nickcollins1995@yahoo.com>
2025-01-16 13:06:06 -0600

    add comments for lines of code

--- Demo1/Program.cs
+++ Demo1/Program.cs
@@ -10,13 +10,16 @@ namespace Demo1
     {
         static void Main(string[] args)
         {
+            // print out the prompt to ask for the name
             Console.WriteLine("What is your name?");
-
+            // declare a string variable to hold the user input
             string name = Console.ReadLine();
-
+            // print out the prompt to ask for the favorite color
             Console.WriteLine("What is your favorite color?");
-
+            // declare a string variable to hold the user input for color
             String color = Console.ReadLine();
+
+
 
         }
     }
